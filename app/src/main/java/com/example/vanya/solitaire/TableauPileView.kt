c81f550a5ad50dad16e2d4c9ab90513108e5997b
VanyaHuaman <vanyahuaman@gmail.com>
2018-08-08 18:37:09 -0500

    Done with CodeLab

---
+++ app/src/main/java/com/example/vanya/solitaire/TableauPileView.kt
@@ -0,0 +1,36 @@
+package com.example.vanya.solitaire
+
+import android.content.Context
+import android.view.ViewManager
+import android.widget.RelativeLayout
+import org.jetbrains.anko.*
+import org.jetbrains.anko.custom.ankoView
+
+class TableauPileView(context: Context, val index : Int) : _RelativeLayout(context){
+
+    init{
+        addViews()
+    }
+
+
+
+    fun update(){
+        removeAllViews()
+        addViews()
+    }
+
+    private fun addViews() {
+        val cards = GameModel.tableauPiles[index].cards
+        cards.forEachIndexed{i, card ->
+            imageView {
+                y = (i * dip(25)).toFloat()
+                imageResource = if (card.faceUp) getResIdForCards(card) else cardBackDrawable
+                onClick {
+                    GamePresenter.onTableauTap(index, i)
+                }
+            }.lparams(context.cardWidth,context.cardHeight)
+        }
+    }
+}
+fun ViewManager.tableauPileView(index: Int, init: TableauPileView.() -> Unit = {}) =
+        ankoView({ TableauPileView(it, index) },0, init)
---
+++ app/src/main/java/com/example/vanya/solitaire/no_android/GameView.kt
@@ -0,0 +1,7 @@
+package com.example.vanya.solitaire
+
+import com.example.vanya.solitaire.GameModel
+
+interface GameView {
+    fun update()
+}
